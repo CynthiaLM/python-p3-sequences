84c2279d1c793c40dc070ca09d34b037aca71405
Laleti Cynthia <+>
2023-10-02 15:54:21 +0300

    lab submission

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    def fibonacci(n):
+        fib_sequence = []
+        a, b = 0, 1
+        for _ in range(n):
+            fib_sequence.append(a)
+            a, b = b, a + b
+        return fib_sequence
+
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = fibonacci(length)
+        print(fib_sequence)
+
+fib_length = 10  
+print_fibonacci(fib_length)
